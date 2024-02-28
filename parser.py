cb3e7219fe76e55d2045286b006741aedf9d5c3d
a.bogomolov <a.bogomolov@icloud.com>
2024-02-28 19:31:10 +0300

    add file quiz and multiple_inputs.py

--- parser.py
+++ parser.py
@@ -1,14 +1,20 @@
 import requests
+from getpass import getpass # Импортируем метод getpass из одноимённой библиотеки для ввода пароля доступа.
+#
+# def test(url):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         print("web is ok")
+#         print(response)
+#     elif response.status_code == 404:
+#         print("web is not ok")
 
 
-def test(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        print("web is ok")
-    elif response.status_code == 404:
-        print("web is not ok")
+
+r = requests.get('https://ironchampion.ru')
 
 
+print(r.text)
+# test('https://ironchampion.ru', params=query)
 
-test('https://ironchampion.ru')
 
